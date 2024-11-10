5fc894af2d731261ad14e1b92298dd245f88713a
FireArmor717 <1621870756@qq.com>
2024-11-10 16:03:27 +0800

    finish part of codes of GeneralPractitioner class

--- Lizhen Wang-A1/src/GeneralPractitioner.java
+++ Lizhen Wang-A1/src/GeneralPractitioner.java
@@ -1,3 +1,12 @@
 public class GeneralPractitioner {
+    private String ConsultationType;
 
+    public GeneralPractitioner(){
+        super();
+        this.ConsultationType = "General Consultation";
+    }
+    public GeneralPractitioner(int id,String name,String department,String ConsultationType){
+        super(id,name,department)
+        this.ConsultationType = ConsultationType;
+    }
 }
